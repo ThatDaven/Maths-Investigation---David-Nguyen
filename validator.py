acd20c82ab875f5ac81176bc91e787a26f735eb2
David Nguyen <57483537+ThatDaven@users.noreply.github.com>
2022-10-31 10:41:54 +0800

    Update validator.py
    Signed-off-by: David Nguyen <57483537+ThatDaven@users.noreply.github.com>

--- validator.py
+++ validator.py
@@ -4,8 +4,8 @@ def validate(x):
       float(x[i])
     except:
       print("ERROR: Enter positive numbers only")
-      return 'false'
+      return False
     if float(x[i]) < 0:
       print("ERROR: Enter positive numbers only")
-      return 'false'
-  return 'true'
+      return False
+  return True
